a90836ce5a50147347a55001505cc29bbe171dc1
case <me@case.codes>
2019-04-08 21:30:32 -0600

    Load lein deps from deps.edn

--- project.clj
+++ project.clj
@@ -1,21 +1,18 @@
+(require '[clojure.edn :as edn])
+
+(def +deps+ (-> "deps.edn" slurp edn/read-string))
+
+(defn deps->vec [deps]
+  (mapv (fn [[dep {:keys [:mvn/version exclusions]}]]
+          (cond-> [dep version]
+            exclusions (conj :exclusions exclusions)))
+        deps))
+
+(def dependencies
+  (deps->vec (:deps +deps+)))
+
 (defproject clojure-lsp "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/tools.reader "1.2.2"]
-                 [org.eclipse.lsp4j/org.eclipse.lsp4j "0.4.0" :exclusions [org.eclipse.xtend/org.eclipse.xtend.lib]]
-                 [org.eclipse.xtend/org.eclipse.xtend.lib "2.13.0" :exclusions [com.google.guava/guava]]
-                 [com.google.guava/guava "19.0"]
-                 [rewrite-clj "0.6.1"]
-                 [log4j/log4j "1.2.17"]
-                 [org.clojure/tools.logging "0.3.1"]
-                 [org.clojure/tools.nrepl "0.2.12"]
-                 [org.clojure/core.async "0.4.474"]
-                 [org.xerial/sqlite-jdbc "3.21.0.1"]
-                 [funcool/clojure.jdbc "0.9.0"]
-                 [digest "1.4.8"]
-                 [cljfmt "0.5.7"]
-                 [medley "1.0.0"]
-                 [com.taoensso/tufte "2.0.1"]
-                 [trptcolin/versioneer "0.2.0"]]
+  :dependencies ~dependencies
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
   :main clojure-lsp.main
   :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.23.0"]

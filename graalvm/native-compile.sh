e7dca32780025a2fa575d594883e349d2775c98a
Eric Dallo <ericdallo06@hotmail.com>
2021-01-31 15:21:37 -0300

    Add Graalvm build CI

--- graalvm/native-compile.sh
+++ graalvm/native-compile.sh
@@ -1,22 +1,47 @@
 #!/usr/bin/env bash
 
-LEIN_SNAPSHOTS_IN_RELEASE=1 lein with-profiles +native-image "do" clean, uberjar
+set -e
 
-native-image \
-    -jar target/*-standalone.jar \
-    -H:Name=clojure-lsp \
-    -J-Dclojure.compiler.direct-linking=true \
-    -J-Dclojure.spec.skip-macros=true \
-    -H:+ReportExceptionStackTraces \
-    --enable-url-protocols=jar \
-    -H:+InlineBeforeAnalysis \
-    -H:Log=registerResource: \
-    --verbose \
-    -H:IncludeResources="db/.*|static/.*|templates/.*|.*.yml|.*.xml|.*/org/sqlite/.*|org/sqlite/.*|.*.properties" \
-    -H:ConfigurationFileDirectories=graalvm \
-    --initialize-at-build-time  \
-    --report-unsupported-elements-at-runtime \
-    --allow-incomplete-classpath \
-    --no-server \
-    --no-fallback \
-    -J-Xmx4g
+if [ -z "$GRAALVM_HOME" ]; then
+    echo "Please set GRAALVM_HOME"
+    exit 1
+fi
+
+if [[ ! -f "$CLOJURE_LSP_JAR" ]]
+then
+    lein with-profiles +native-image "do" clean, uberjar
+    CLOJURE_LSP_JAR=$(ls target/clojure-lsp-*-standalone.jar)
+fi
+
+CLOJURE_LSP_XMX=${CLOJURE_LSP_XMX:-"-J-Xmx4g"}
+
+"$GRAALVM_HOME/bin/gu" install native-image
+
+args=("-jar" "$CLOJURE_LSP_JAR"
+      "-H:Name=clojure-lsp"
+      "-J-Dclojure.compiler.direct-linking=true"
+      "-J-Dclojure.spec.skip-macros=true"
+      "-H:+ReportExceptionStackTraces"
+      "--enable-url-protocols=jar"
+      "-H:+InlineBeforeAnalysis"
+      "-H:Log=registerResource:"
+      "--verbose"
+      "-H:IncludeResources='db/.*|static/.*|templates/.*|.*.yml|.*.xml|.*/org/sqlite/.*|org/sqlite/.*|.*.properties'"
+      "-H:ConfigurationFileDirectories=graalvm"
+      "--initialize-at-build-time"
+      "--report-unsupported-elements-at-runtime"
+      "--no-server"
+      "--no-fallback"
+      "--native-image-info"
+      "--allow-incomplete-classpath"
+      "-H:ServiceLoaderFeatureExcludeServices=javax.sound.sampled.spi.AudioFileReader"
+      "-H:ServiceLoaderFeatureExcludeServices=javax.sound.midi.spi.MidiFileReader"
+      "-H:ServiceLoaderFeatureExcludeServices=javax.sound.sampled.spi.MixerProvider"
+      "-H:ServiceLoaderFeatureExcludeServices=javax.sound.sampled.spi.FormatConversionProvider"
+      "-H:ServiceLoaderFeatureExcludeServices=javax.sound.sampled.spi.AudioFileWriter"
+      "-H:ServiceLoaderFeatureExcludeServices=javax.sound.midi.spi.MidiDeviceProvider"
+      "-H:ServiceLoaderFeatureExcludeServices=javax.sound.midi.spi.SoundbankReader"
+      "-H:ServiceLoaderFeatureExcludeServices=javax.sound.midi.spi.MidiFileWriter"
+      "$CLOJURE_LSP_XMX")
+
+"$GRAALVM_HOME/bin/native-image" "${args[@]}"

1c8c1b6f35bc69297eb64fa99da50be8f421e907
Eric Dallo <ericdallo06@hotmail.com>
2021-02-04 22:23:30 -0300

    [Dev] Fix nrepl dependency creating a debug profile

--- project.clj
+++ project.clj
@@ -12,18 +12,19 @@
 (def dependencies
   (deps->vec (:deps +deps+)))
 
-(defproject clojure-lsp #=(clojure.string/trim
-                            #=(slurp "resources/CLOJURE_LSP_VERSION"))
+(defproject clojure-lsp
+  #=(clojure.string/trim
+      #=(slurp "resources/CLOJURE_LSP_VERSION"))
   :dependencies ~dependencies
   :jvm-opts ^:replace ["-Xmx2g" "-server"]
   :main clojure-lsp.main
   :java-source-paths ["src-java"]
   :resource-paths ["resources"]
   :javac-options ["-implicit:none"]
+  :bin {:name "clojure-lsp"}
   :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]
-                             [lein-binplus "0.6.6"]]
-                   :bin {:name "clojure-lsp"}
-                   :dependencies [[nrepl/nrepl "0.8.3"]]}
+                             [lein-binplus "0.6.6"]]}
+             :debug ^:leaky {:dependencies [[nrepl/nrepl "0.8.3"]]}
              :test {:test-selectors {:focused :focused}
                     :dependencies [[pjstadig/humane-test-output "0.9.0"]]
                     :injections [(require 'pjstadig.humane-test-output)

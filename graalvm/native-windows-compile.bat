87cdfb4b6d6172ac39c8b9bb82ef8f37d6d27245
Eric Dallo <ericdallo06@hotmail.com>
2021-02-13 14:06:02 -0300

    [CI] Fix windows release

--- graalvm/native-windows-compile.bat
+++ graalvm/native-windows-compile.bat
@@ -31,5 +31,7 @@ call %GRAALVM_HOME%\bin\native-image.cmd ^
 
 if %errorlevel% neq 0 exit /b %errorlevel%
 
+dir
+
 rem graalvm ignores -H:Name for some reason and use rem.exe as the binary name
 ren "rem.exe" "clojure-lsp.exe"

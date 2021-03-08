0f0382de91ede9a67b7fff72c221375e66160735
Eric Dallo <ericdallo06@hotmail.com>
2021-03-08 10:17:23 -0300

    [graalvm] Move graalvm configs to jar

--- graalvm/native-unix-compile.sh
+++ graalvm/native-unix-compile.sh
@@ -16,31 +16,10 @@ fi
 CLOJURE_LSP_XMX=${CLOJURE_LSP_XMX:-"-J-Xmx4g"}
 
 args=("-jar" "$CLOJURE_LSP_JAR"
-      "-H:Name=clojure-lsp"
-      "-J-Dborkdude.dynaload.aot=true"
-      "-J-Dclojure.compiler.direct-linking=true"
-      "-J-Dclojure.spec.skip-macros=true"
       "-H:+ReportExceptionStackTraces"
-      "--enable-url-protocols=jar"
-      "-H:+InlineBeforeAnalysis"
-      "-H:Log=registerResource:"
       "--verbose"
-      "-H:IncludeResources=\"CLOJURE_LSP_VERSION|db/.*|static/.*|templates/.*|.*.yml|.*.xml|.*/org/sqlite/.*|org/sqlite/.*|.*.properties\""
-      "-H:ConfigurationFileDirectories=graalvm"
-      "--initialize-at-build-time"
-      "--report-unsupported-elements-at-runtime"
-      "--no-server"
       "--no-fallback"
       "--native-image-info"
-      "--allow-incomplete-classpath"
-      "-H:ServiceLoaderFeatureExcludeServices=javax.sound.sampled.spi.AudioFileReader"
-      "-H:ServiceLoaderFeatureExcludeServices=javax.sound.midi.spi.MidiFileReader"
-      "-H:ServiceLoaderFeatureExcludeServices=javax.sound.sampled.spi.MixerProvider"
-      "-H:ServiceLoaderFeatureExcludeServices=javax.sound.sampled.spi.FormatConversionProvider"
-      "-H:ServiceLoaderFeatureExcludeServices=javax.sound.sampled.spi.AudioFileWriter"
-      "-H:ServiceLoaderFeatureExcludeServices=javax.sound.midi.spi.MidiDeviceProvider"
-      "-H:ServiceLoaderFeatureExcludeServices=javax.sound.midi.spi.SoundbankReader"
-      "-H:ServiceLoaderFeatureExcludeServices=javax.sound.midi.spi.MidiFileWriter"
       "$CLOJURE_LSP_XMX")
 
 CLOJURE_LSP_STATIC=${CLOJURE_LSP_STATIC:-}

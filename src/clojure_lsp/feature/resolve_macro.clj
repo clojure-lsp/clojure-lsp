7c29c31c6aaf5076e3465a3ca57074306ae54239
Eric Dallo <ericdallo06@hotmail.com>
2021-03-21 12:02:03 -0300

    Use rewrite-edn to update the config

--- src/clojure_lsp/feature/resolve_macro.clj
+++ src/clojure_lsp/feature/resolve_macro.clj
@@ -1,5 +1,6 @@
 (ns clojure-lsp.feature.resolve-macro
   (:require
+   [borkdude.rewrite-edn :as r]
    [clojure-lsp.db :as db]
    [clojure-lsp.feature.file-management :as f.file-management]
    [clojure-lsp.parser :as parser]
@@ -7,7 +8,6 @@
    [clojure-lsp.refactor.edit :as edit]
    [clojure-lsp.shared :as shared]
    [clojure.java.io :as io]
-   [rewrite-clj.node :as n]
    [rewrite-clj.zip :as z]
    [taoensso.timbre :as log]))
 
@@ -27,76 +27,12 @@
     (when (:macro definition)
       (symbol (-> definition :ns name) (-> definition :name name)))))
 
-(defn ^:private add-lint-as-config [config-loc resolved-full-symbol full-symbol]
-  (let [spaces-count (some-> config-loc
-                             z/down
-                             z/node
-                             meta
-                             :col
-                             dec)]
-    (-> config-loc
-        (z/edit->
-         ((fn [loc] (cond-> loc
-                      spaces-count
-                      (-> z/down
-                          z/rightmost
-                          (z/insert-right (n/whitespace-node (apply str (repeat spaces-count " "))))
-                          (z/insert-right (n/newlines 1))
-                          z/up)))))
-        (z/assoc :lint-as {full-symbol resolved-full-symbol})
-        z/string
-        (str "\n"))))
-
-(defn ^:private update-lint-as-config [config-loc spaces-node resolved-full-symbol full-symbol]
+(defn ^:private update-macro-resolve-for-config
+  [resolved-full-symbol full-symbol config-loc]
   (-> config-loc
-      (z/edit->
-       z/down
-       (z/find-next-value :lint-as)
-       z/next
-       ((fn [loc] (cond-> loc
-                    spaces-node
-                    (-> z/down
-                        z/rightmost
-                        (z/insert-right spaces-node)
-                        (z/insert-right (n/newlines 1))
-                        z/up))))
-       (z/assoc full-symbol resolved-full-symbol))
-      z/string
+      z/node
+      (r/assoc-in [:lint-as full-symbol] resolved-full-symbol)
       (str "\n")))
-
-(defn ^:private update-macro-resolve-for-config [resolved-full-symbol full-symbol config-loc]
-  (let [lint-as-exists? (-> config-loc
-                            z/down
-                            (z/find-next-value :lint-as)
-                            boolean)
-        add-spaces? (and (-> config-loc
-                             z/down
-                             (z/find-next-value :lint-as)
-                             z/next
-                             z/sexpr
-                             seq)
-                         (-> config-loc
-                             z/down
-                             (z/find-next-value :lint-as)
-                             z/next
-                             z/down
-                             (z/find-next-value full-symbol)
-                             boolean
-                             not))
-        spaces-count (when add-spaces?
-                       (-> config-loc
-                           z/down
-                           (z/find-next-value :lint-as)
-                           z/next
-                           z/node
-                           meta
-                           :col
-                           dec))
-        spaces-node (when add-spaces?
-                      (n/whitespace-node (apply str (repeat spaces-count " "))))]
-    (if lint-as-exists?
-      (update-lint-as-config config-loc spaces-node resolved-full-symbol full-symbol)
-      (add-lint-as-config config-loc resolved-full-symbol full-symbol))))
 
 (defn resolve-macro-as [uri row col resolved-full-symbol-str kondo-config-path]
   (let [full-symbol (full-symbol-to-resolve uri row col)

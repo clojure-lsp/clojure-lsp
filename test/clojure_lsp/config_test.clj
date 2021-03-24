1a9ad9b778759722db6e97a31e3bf4f9c78af895
Eric Dallo <ericdallo06@hotmail.com>
2021-03-24 09:25:21 -0300

    Fix test

--- test/clojure_lsp/config_test.clj
+++ test/clojure_lsp/config_test.clj
@@ -6,6 +6,7 @@
 (deftest resolve-config
   (testing "when user doesn't have a home config or a project config"
     (with-redefs [config/get-property (constantly nil)
+                  config/file-exists? (constantly nil)
                   config/get-env (constantly nil)]
       (is (= {} (config/resolve-config "/home/user/some/project/")))))
   (testing "when user doesn't have a home config but has a project config"

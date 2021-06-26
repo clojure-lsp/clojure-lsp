f41bf46b129296d493e5f5f4dc7c80c6b488eb8d
Eric Dallo <ericdallo06@hotmail.com>
2021-06-26 18:19:06 -0300

    Validate --namespace

--- src/clojure_lsp/internal_api.clj
+++ src/clojure_lsp/internal_api.clj
@@ -19,9 +19,9 @@
        :text-document-sync-kind :full})))
 
 (defn ^:private ns->uri [namespace]
-  (let [source-paths (-> @db/db :settings :source-paths)
-        filename (:filename (q/find-namespace-definition-by-namespace (:analysis @db/db) namespace))]
-    (shared/namespace->uri (name namespace) source-paths filename)))
+  (let [source-paths (-> @db/db :settings :source-paths)]
+    (when-let [filename (:filename (q/find-namespace-definition-by-namespace (:analysis @db/db) namespace))]
+      (shared/namespace->uri (name namespace) source-paths filename))))
 
 (defn ^:private open-file! [uri]
   (handlers/did-open {:textDocument {:uri uri :text (slurp uri)}}))
@@ -33,9 +33,11 @@
                          q/filter-project-analysis
                          q/find-all-ns-definitions))]
     (doseq [namespace namespaces]
-      (let [uri (ns->uri namespace)]
-        (open-file! uri)
-        (when-let [edits (handlers/execute-command {:command "clean-ns"
-                                                    :arguments [uri 0 0]})]
-          (when (seq (client/apply-workspace-edits edits))
-            (println "Cleaned" namespace)))))))
+      (if-let [uri (ns->uri namespace)]
+        (do
+          (open-file! uri)
+          (when-let [edits (handlers/execute-command {:command "clean-ns"
+                                                      :arguments [uri 0 0]})]
+            (when (seq (client/apply-workspace-edits edits))
+              (println "Cleaned" namespace))))
+        (println "Namespace" namespace "not found")))))

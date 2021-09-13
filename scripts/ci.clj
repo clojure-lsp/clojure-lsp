291f4d5eb98094e04de697408f63a49a573303e6
Eric Dallo <ercdll1337@gmail.com>
2021-09-13 11:13:38 -0300

    [CI] Add changelog text to new releases (#550)
    * [CI] Add changelog text to new releases

---
+++ scripts/ci.clj
@@ -0,0 +1,15 @@
+(ns ci
+  (:require [clojure.string :as string]))
+
+(defn ^:private make-literal [a]
+  (.replace a "\"" "\\\""))
+
+(defn ^:private extract-text-between [prefix suffix from-string]
+  (let [pattern (str (make-literal prefix) "([\\s\\S]*?)" (make-literal suffix))]
+    (second (re-find (re-pattern pattern) from-string))))
+
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
+(defn get-last-changelog-entry [version]
+  (println (->> (slurp "CHANGELOG.md")
+                (extract-text-between (str "## " version) "## ")
+                string/trim)))

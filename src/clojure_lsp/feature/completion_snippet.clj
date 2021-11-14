3d6cf5569ba5b793792923ea01f01f11e27b425b
Eric Dallo <ericdallo06@hotmail.com>
2021-11-14 19:48:21 -0300

    Improve snippets

--- src/clojure_lsp/feature/completion_snippet.clj
+++ src/clojure_lsp/feature/completion_snippet.clj
@@ -10,12 +10,12 @@
 (defn known-snippets [settings]
   [{:label "comment"
     :detail "Create comment block"
-    :insert-text "(comment\n  $0\n  )"}
+    :insert-text "(comment\n  ${0:body}\n  )"}
    {:label "condp"
     :detail "Create condp"
-    :insert-text "(condp ${1:pred} ${2:expr}\n $0)"}
+    :insert-text "(condp ${1:pred} ${2:expr}\n ${0:clauses})"}
    {:label "def"
-    :insert-text "(def ${1:name} $0)"
+    :insert-text "(def ${1:name} ${0:value})"
     :detail "Create def"}
    {:label "defmethod"
     :detail "Create defmethod"
@@ -25,25 +25,25 @@
     :insert-text "(defmulti ${1:name} ${2:dispatch-fn})"}
    {:label "defn-"
     :detail "Create private function"
-    :insert-text (format "(defn%s ${1:name} [$2]\n  $0)"
+    :insert-text (format "(defn%s ${1:name} [$2]\n  ${0:body})"
                          (if (:use-metadata-for-privacy? settings)
                            " ^:private"
                            "-"))}
    {:label "defn"
-    :insert-text "(defn ${1:foo} [$2]\n  $0)"
+    :insert-text "(defn ${1:name} [$2]\n  ${0:body})"
     :detail "Create public function"}
    {:label "defprotocol"
     :detail "Create defprotocol"
-    :insert-text "(defprotocol ${1:Name}\n $0)"}
+    :insert-text "(defprotocol ${1:Name}\n ${0:body})"}
    {:label "defrecord"
     :detail "Create defrecord"
-    :insert-text "(defrecord ${1:Name} [${2:fields}]\n ${3:Protocol}\n $0)"}
+    :insert-text "(defrecord ${1:Name} [${2:fields}]\n ${3:Protocol}\n ${0:body})"}
    {:label "deftype"
     :detail "Create deftype"
-    :insert-text "(deftype ${1:Name} [${2:fields}]\n ${3:Protocol}\n $0)"}
+    :insert-text "(deftype ${1:Name} [${2:fields}]\n ${3:Protocol}\n ${0:body})"}
    {:label "fn"
     :detail "Create fn"
-    :insert-text "(fn [${1:arg-list}] $0)"}
+    :insert-text "(fn [${1:arg-list}] ${0:body})"}
    {:label "if"
     :detail "Create if"
     :insert-text "(if ${1:test-expr}\n ${2:then-expr}\n ${3:else-expr})"}
@@ -55,16 +55,16 @@
     :insert-text "& {:keys [${1:keys}] :or {${2:defaults}}}"}
    {:label "let"
     :detail "Create let"
-    :insert-text "(let [$0])"}
+    :insert-text "(let [${1:binding} ${2:value}])"}
    {:label "letfn"
     :detail "Create letfn"
-    :insert-text "(letfn [(${1:name} [${2:args}]\n $0)])"}
+    :insert-text "(letfn [(${1:name} [${2:args}]\n ${0:body})])"}
    {:label "ns"
     :detail "Create ns"
-    :insert-text "(ns $1\n  $0)"}
+    :insert-text "(ns ${1:name}\n  ${0:references})"}
    {:label "require"
     :detail "Create require"
-    :insert-text "(:require [${1:namespace} :as [$0]])"}
+    :insert-text "(:require [${1:namespace} :as ${0:alias}])"}
    {:label "use"
     :detail "Create use"
     :insert-text "(:use [${1:namespace} :only [$0]])"}])

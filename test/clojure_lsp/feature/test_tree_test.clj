e03ffa9904dea2c1161501491912ed64c876b4b2
Eric Dallo <ericdallo06@hotmail.com>
2021-12-11 19:29:43 -0300

    Bump kondo

--- test/clojure_lsp/feature/test_tree_test.clj
+++ test/clojure_lsp/feature/test_tree_test.clj
@@ -24,37 +24,44 @@
                                   ")"))
     (h/assert-submap
       {:uri "file:///a.clj"
-       :tests
-       [{:name "some-test"
-         :range {:start {:line 1 :character 0}
-                 :end {:line 8 :character 1}}
-         :name-range {:start {:line 1 :character 9}
-                      :end {:line 1 :character 18}}
-         :kind :deftest
-         :children
-         [{:name "foo"
-           :range {:start {:line 2 :character 2}
-                   :end {:line 3 :character 12}}
-           :name-range {:start {:line 2 :character 3}
-                        :end {:line 2 :character 10}}
-           :kind :testing}
-          {:name "bar baz"
-           :range {:start {:line 4 :character 2}
-                   :end {:line 7 :character 15}}
-           :name-range {:start {:line 4 :character 3}
-                        :end {:line 4 :character 10}}
-           :kind :testing
-           :children
-           [{:name "qux"
-             :range {:start {:line 5 :character 4}
-                     :end {:line 5 :character 23}}
-             :name-range {:start {:line 5 :character 5}
-                          :end {:line 5 :character 12}}
-             :kind :testing}
-            {:name "last one"
-             :range {:start {:line 6 :character 4}
-                     :end {:line 7 :character 14}}
-             :name-range {:start {:line 6 :character 5}
-                          :end {:line 6 :character 12}}
-             :kind :testing}]}]}]}
+       :tree
+       {:name "foo.bar"
+        :range {:start {:line 0 :character 0}
+                :end {:line 0 :character 11}}
+        :name-range {:start {:line 0 :character 4}
+                     :end {:line 0 :character 11}}
+        :kind :namespace
+        :children
+        [{:name "some-test"
+          :range {:start {:line 1 :character 0}
+                  :end {:line 8 :character 1}}
+          :name-range {:start {:line 1 :character 9}
+                       :end {:line 1 :character 18}}
+          :kind :deftest
+          :children
+          [{:name "foo"
+            :range {:start {:line 2 :character 2}
+                    :end {:line 3 :character 12}}
+            :name-range {:start {:line 2 :character 3}
+                         :end {:line 2 :character 10}}
+            :kind :testing}
+           {:name "bar baz"
+            :range {:start {:line 4 :character 2}
+                    :end {:line 7 :character 15}}
+            :name-range {:start {:line 4 :character 3}
+                         :end {:line 4 :character 10}}
+            :kind :testing
+            :children
+            [{:name "qux"
+              :range {:start {:line 5 :character 4}
+                      :end {:line 5 :character 23}}
+              :name-range {:start {:line 5 :character 5}
+                           :end {:line 5 :character 12}}
+              :kind :testing}
+             {:name "last one"
+              :range {:start {:line 6 :character 4}
+                      :end {:line 7 :character 14}}
+              :name-range {:start {:line 6 :character 5}
+                           :end {:line 6 :character 12}}
+              :kind :testing}]}]}]}}
       (f.test-tree/tree "file:///a.clj" db/db))))

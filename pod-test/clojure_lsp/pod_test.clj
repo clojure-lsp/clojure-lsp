2652bca6fcc8b601dc19e7d7d9af1b29774cb77b
Eric Dallo <ericdallo06@hotmail.com>
2021-12-18 12:45:22 -0300

    Add babashka pod

---
+++ pod-test/clojure_lsp/pod_test.clj
@@ -0,0 +1,20 @@
+(ns clojure-lsp.pod-test
+  (:require
+   [babashka.pods :as pods]
+   [clojure.java.io :as io]
+   [clojure.test :refer [deftest is testing]]))
+
+(def pod-spec (if (= "native" (System/getenv "CLOJURE_LSP_TEST_ENV"))
+                ["./clojure-lsp"]
+                ["clojure" "-M:run"]))
+
+(pods/load-pod pod-spec)
+(require '[pod.ericdallo.clojure-lsp :as clojure-lsp])
+
+#_{:clj-kondo/ignore [:unresolved-var]}
+(deftest pod-test
+  (testing "format"
+    (is (clojure-lsp/format! {:project-root (io/file "integration-test/sample-test")
+                              :namespace '[sample-test.api.format.a]
+                              :raw? true
+                              :dry? true}))))

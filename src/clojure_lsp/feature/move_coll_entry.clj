7f1d92349aa6d039e8e316e75823009dc4955f49
Jacob Maine <jacob.maine@gmail.com>
2022-01-07 12:58:03 -0800

    Improve legibility of move-coll-entry

--- src/clojure_lsp/feature/move_coll_entry.clj
+++ src/clojure_lsp/feature/move_coll_entry.clj
@@ -2,10 +2,25 @@
   (:require
    [clojure.string :as string]
    [rewrite-clj.node :as n]
-   [rewrite-clj.zip :as z]
-   [taoensso.timbre :as log]))
+   [rewrite-clj.zip :as z]))
 
 (set! *warn-on-reflection* true)
+
+(defn- count-siblings-left
+  "Count the number of sibling nodes to the left of the child node `zloc`."
+  [zloc]
+  (->> zloc
+       (iterate z/left)
+       (take-while (complement z/leftmost?))
+       count))
+
+(defn- count-siblings-right
+  "Count the number of sibling nodes to the right of the child node `zloc`."
+  [zloc]
+  (->> zloc
+       (iterate z/right)
+       (take-while (complement z/rightmost?))
+       count))
 
 (defn ^:private expand-comment-newlines
   "Comment nodes often include their trailing newline. This splits such nodes
@@ -19,11 +34,9 @@
               [n]))
           children))
 
-(defn ^:private whitespace-comment-or-newline? [node]
-  (contains? #{:whitespace :comment :newline} (n/tag node)))
-
-(defn ^:private whitespace-or-comment? [node]
-  (contains? #{:whitespace :comment} (n/tag node)))
+(defn- trailing-whitespace? [node]
+  (and (n/whitespace-or-comment? node)
+       (not (n/linebreak? node))))
 
 (defn ^:private parse-entry-pairs
   "Parse the children of a map or binding `parent-zloc` into entry pairs
@@ -57,13 +70,13 @@
             ;; everything processed
             [result []]
 
-            (every? whitespace-comment-or-newline? children)
+            (every? n/whitespace-or-comment? children)
             ;; everything processed but extra lines
             [result children]
 
             (= :before-key state)
             ;; gather comments before key, and key itself
-            (let [[prefix [k & rst]] (split-with whitespace-comment-or-newline?
+            (let [[prefix [k & rst]] (split-with n/whitespace-or-comment?
                                                  children)]
               (recur rst
                      :before-val
@@ -72,9 +85,9 @@
             (= :before-val state)
             ;; gather comments before val, val itself, and optional comment
             ;; after val on same line
-            (let [[prefix [v & rst]] (split-with whitespace-comment-or-newline?
+            (let [[prefix [v & rst]] (split-with n/whitespace-or-comment?
                                                  children)
-                  [postfix rst]      (split-with whitespace-or-comment?
+                  [postfix rst]      (split-with trailing-whitespace?
                                                  rst)]
               (recur rst
                      :before-key
@@ -87,20 +100,18 @@
 (defn ^:private move-entry-zloc
   "Move `zloc` to direction `dir` considering multiple comments cases."
   [zloc dir]
-  (let [parent-zloc (z/up zloc)
+  (let [parent-zloc               (z/up zloc)
         [entry-pairs extra-lines] (parse-entry-pairs parent-zloc)
-        node-position     (->> zloc
-                               (iterate z/left)
-                               (take-while (complement z/leftmost?))
-                               count)
-        new-node-position (dir (dir node-position))
-        ;; position of the entry pair we're moving, whether the original
+
+        node-idx     (count-siblings-left zloc)
+        new-node-idx (dir (dir node-idx))
+        ;; index of the entry pair we're moving, whether the original
         ;; node was a key or value.
-        position          (-> node-position (/ 2) int)
-        new-position      (dir position)
-        entry-pairs (assoc entry-pairs
-                           position (get entry-pairs new-position)
-                           new-position (get entry-pairs position))
+        pair-idx     (-> node-idx (/ 2) int)
+        new-pair-idx (dir pair-idx)
+        entry-pairs  (assoc entry-pairs
+                            pair-idx (get entry-pairs new-pair-idx)
+                            new-pair-idx (get entry-pairs pair-idx))
         ;; Most entries can keep the whitespace that precedes them,
         ;; whether moving up or down. This assumption fails when
         ;; swapping the first and second entries. The entry that is
@@ -113,23 +124,28 @@
         ;; TODO: seems to work whether or not we're swapping the first
         ;; and second entries. But is that accounting for all whitespace
         ;; edge cases? Maybe better to do this only if (= #{0 1}
-        ;; (hash-set position new-position)).
-        entry-pairs (-> entry-pairs
-                        (update 0 #(drop-while n/whitespace? %))
-                        (update 1 #(concat (take-while n/whitespace? (get entry-pairs 0))
-                                           %)))
+        ;; (hash-set pair-idx new-pair-idx)).
+        [whitespace first-entry] (split-with n/whitespace? (get entry-pairs 0))
+        entry-pairs              (-> entry-pairs
+                                     (assoc 0 first-entry)
+                                     (update 1 #(concat whitespace %)))
 
         ;; Put revised entries back into parent.
         parent-zloc (z/subedit-> parent-zloc
                                  (z/replace (n/replace-children (z/node parent-zloc)
                                                                 (concat (flatten entry-pairs)
                                                                         extra-lines))))
-        parent-zloc (if (-> parent-zloc z/down z/rightmost* z/node n/comment?)
-                      ;; Do we really need to use z/position and add track-position to whole clojure-lsp zloc to make this work?
-                      (let [[_row col] (-> parent-zloc z/down z/rightmost z/left z/position)]
-                        (-> parent-zloc
-                            z/down
-                            z/rightmost*
+
+        ;; If after reordering, the last component has become a comment,
+        ;; we need to add a newline or else the closing bracket will be
+        ;; commented out.
+        last-zloc   (-> parent-zloc z/down z/rightmost*)
+        parent-zloc (if (-> last-zloc z/node n/comment?)
+                      ;; align bracket with last key
+                      (let [last-key   (-> parent-zloc z/down z/rightmost z/left)
+                            ;; Do we really need to use z/position and add track-position to whole clojure-lsp zloc to make this work?
+                            [_row col] (z/position last-key)]
+                        (-> last-zloc
                             (z/insert-space-right (dec col))
                             z/insert-newline-right
                             z/up))
@@ -139,7 +155,7 @@
     (->> parent-zloc
          z/down
          (iterate z/right)
-         (drop new-node-position)
+         (drop new-node-idx)
          first)))
 
 (defn ^:private can-move-entry? [zloc]
@@ -148,19 +164,11 @@
 
 (defn can-move-entry-up? [zloc]
   (and (can-move-entry? zloc)
-       (>= (->> zloc
-                (iterate z/left)
-                (take-while (complement z/leftmost?))
-                count)
-           2)))
+       (>= (count-siblings-left zloc) 2)))
 
 (defn can-move-entry-down? [zloc]
   (and (can-move-entry? zloc)
-       (>= (->> zloc
-                (iterate z/right)
-                (take-while (complement z/rightmost?))
-                count)
-           2)))
+       (>= (count-siblings-right zloc) 2)))
 
 (defn ^:private move-entry [zloc uri dir focus-zloc]
   (when-let [new-zloc (move-entry-zloc zloc dir)]

f164722abcbb11aa8b048d8a7f539a41311015d7
Eric Dallo <ericdallo06@hotmail.com>
2022-01-15 20:15:16 -0300

    Fix tests

--- cli/integration-test/integration/api/version_test.clj
+++ cli/integration-test/integration/api/version_test.clj
@@ -9,7 +9,7 @@
 (deftest version
   (testing "Checking --version"
     (with-open [rdr (lsp/cli! "--version")]
-      (let [current-version (string/trim (slurp "resources/CLOJURE_LSP_VERSION"))
+      (let [current-version (string/trim (slurp "../lib/resources/CLOJURE_LSP_VERSION"))
             output (slurp rdr)]
         (is (string/includes? output (format "clojure-lsp %s" current-version)))
         (is (string/includes? output "clj-kondo"))))))

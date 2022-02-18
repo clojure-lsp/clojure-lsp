75850adc38704dbf698367a91bd00311c843d1ce
Jacob Maine <jacob.maine@gmail.com>
2022-02-17 17:04:52 -0800

    Add integration test for completing clojure.core syms

--- cli/integration-test/integration/completion_test.clj
+++ cli/integration-test/integration/completion_test.clj
@@ -1,6 +1,6 @@
 (ns integration.completion-test
   (:require
-   [clojure.test :refer [deftest testing]]
+   [clojure.test :refer [deftest is testing]]
    [integration.fixture :as fixture]
    [integration.helper :as h]
    [integration.lsp :as lsp]))
@@ -36,12 +36,24 @@
       (h/assert-contains-submaps
        [{:label "defn"
          :kind 15
-         :detail "clojure.core/defn" 
+         :detail "clojure.core/defn"
          :insertText "(defn ${1:name} [$2]\n  ${0:body})"
          :insertTextFormat 2
          :data {:filename "/clojure.core.clj" :name "defn" :ns "clojure.core", :snippet-kind 6}}]
        (lsp/request! (fixture/completion-request "completion/a.clj" 2 4)))))
-  (testing "completions in whitespace"
+  (testing "completions from whitespace"
+    (let [completions (lsp/request! (fixture/completion-request "completion/a.clj" 3 1))]
+      (is (< 800 (count completions)))
+      (h/assert-contains-submaps
+        [{:label "Map"
+          :kind 7
+          :detail "java.util.Map"}
+         {:label "vec"
+          :kind 3
+          :detail "clojure.core/vec"
+          :data {:filename "/clojure.core.clj", :name "vec", :ns "clojure.core"}}]
+        completions)))
+  (testing "completions from comment"
     (h/assert-submaps
       []
       (lsp/request! (fixture/completion-request "completion/a.clj" 4 3)))))

2e9860a311bf2f8fdc0b9ec1ad5e2fa1bd49e34e
Eric Dallo <ericdallo06@hotmail.com>
2022-03-13 18:49:42 -0300

    fix integration tests

--- cli/integration-test/integration/linked_editing_range_test.clj
+++ cli/integration-test/integration/linked_editing_range_test.clj
@@ -11,17 +11,17 @@
   (lsp/start-process!)
   (lsp/request! (fixture/initialize-request))
   (lsp/notify! (fixture/initialized-notification))
-  (lsp/notify! (fixture/did-open-notification "linked_editing_shared/a.cljc"))
-  (lsp/notify! (fixture/did-open-notification "linked_editing_shared/b.cljc"))
+  (lsp/notify! (fixture/did-open-notification "linked_editing_range/a.cljc"))
+  (lsp/notify! (fixture/did-open-notification "linked_editing_range/b.cljc"))
 
   (testing "Renaming locals"
     (h/assert-submap
       {:ranges  [{:start {:line 2 :character 11} :end {:line 2 :character 15}}
                  {:start {:line 3 :character 13} :end {:line 3 :character 17}}
                  {:start {:line 4 :character 9} :end {:line 4 :character 13}}]}
-      (lsp/request! (fixture/linked-editing-range-request "linked_editing_shared/a.cljc" 2 13))))
+      (lsp/request! (fixture/linked-editing-range-request "linked_editing_range/a.cljc" 2 13))))
 
   (testing "Renaming function"
     (h/assert-submap
       {:error {:code -32602, :message "There are references on other files for this symbol"}}
-      (lsp/request! (fixture/linked-editing-range-request "linked_editing_shared/a.cljc" 2 7)))))
+      (lsp/request! (fixture/linked-editing-range-request "linked_editing_range/a.cljc" 2 7)))))

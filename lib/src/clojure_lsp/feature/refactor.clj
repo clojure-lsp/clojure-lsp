b959d79cc4b60ee8ffd7d7fe5f4d22cc51790e6c
Jacob Maine <jacob.maine@gmail.com>
2022-04-03 11:49:23 -0700

    Pass derefed db to refactor.refactor-client-seq-changes

--- lib/src/clojure_lsp/feature/refactor.clj
+++ lib/src/clojure_lsp/feature/refactor.clj
@@ -107,11 +107,10 @@
        (map name)
        vec))
 
-;; TODO: deref
 (defn refactor-client-seq-changes [uri version result db]
   (let [changes [{:text-document {:uri uri :version version}
                   :edits (mapv #(medley/update-existing % :range shared/->range) (r.transform/result result))}]]
-    (shared/client-changes changes @db)))
+    (shared/client-changes changes db)))
 
 (defn call-refactor [{:keys [loc uri refactoring row col version] :as data} {:keys [db] :as components}]
 
@@ -137,7 +136,7 @@
          :edit (shared/client-changes changes @db)})
 
       (seq result)
-      {:edit (refactor-client-seq-changes uri version result db)}
+      {:edit (refactor-client-seq-changes uri version result @db)}
 
       (empty? result)
       (logger/warn refactoring "made no changes" (z/string loc))

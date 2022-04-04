fceaf3b20e0d4d96b282447b0dd512a76f4fa3dd
Jacob Maine <jacob.maine@gmail.com>
2022-04-03 18:21:21 -0700

    Deref outside of refactor

--- lib/src/clojure_lsp/feature/refactor.clj
+++ lib/src/clojure_lsp/feature/refactor.clj
@@ -16,20 +16,20 @@
 
 (defmulti refactor :refactoring)
 
-(defmethod refactor :add-import-to-namespace [{:keys [loc uri args] {:keys [db*]} :components}]
-  (apply f.add-missing-libspec/add-missing-import loc uri (concat args [@db*])))
+(defmethod refactor :add-import-to-namespace [{:keys [loc uri args db]}]
+  (apply f.add-missing-libspec/add-missing-import loc uri (concat args [db])))
 
-(defmethod refactor :add-missing-libspec [{:keys [loc uri] {:keys [db*]} :components}]
-  (f.add-missing-libspec/add-missing-libspec loc uri @db*))
+(defmethod refactor :add-missing-libspec [{:keys [loc uri db]}]
+  (f.add-missing-libspec/add-missing-libspec loc uri db))
 
-(defmethod refactor :add-require-suggestion [{:keys [loc uri args] {:keys [db*]} :components}]
-  (apply f.add-missing-libspec/add-require-suggestion loc uri (concat args [@db*])))
+(defmethod refactor :add-require-suggestion [{:keys [loc uri args db]}]
+  (apply f.add-missing-libspec/add-require-suggestion loc uri (concat args [db])))
 
-(defmethod refactor :add-missing-import [{:keys [loc uri] {:keys [db*]} :components}]
-  (f.add-missing-libspec/add-missing-import loc uri nil @db*))
+(defmethod refactor :add-missing-import [{:keys [loc uri db]}]
+  (f.add-missing-libspec/add-missing-import loc uri nil db))
 
-(defmethod refactor :clean-ns [{:keys [loc uri] {:keys [db*]} :components}]
-  (f.clean-ns/clean-ns-edits loc uri @db*))
+(defmethod refactor :clean-ns [{:keys [loc uri db]}]
+  (f.clean-ns/clean-ns-edits loc uri db))
 
 (defmethod refactor :cycle-coll [{:keys [loc]}]
   (r.transform/cycle-coll loc))
@@ -37,38 +37,38 @@
 (defmethod refactor :change-coll [{:keys [loc args]}]
   (apply r.transform/change-coll loc args))
 
-(defmethod refactor :cycle-privacy [{:keys [loc] {:keys [db*]} :components}]
-  (r.transform/cycle-privacy loc @db*))
+(defmethod refactor :cycle-privacy [{:keys [loc db]}]
+  (r.transform/cycle-privacy loc db))
 
 (defmethod refactor :cycle-fn-literal [{:keys [loc]}]
   (r.transform/cycle-fn-literal loc))
 
-(defmethod refactor :expand-let [{:keys [loc uri] {:keys [db*]} :components}]
-  (r.transform/expand-let loc uri @db*))
+(defmethod refactor :expand-let [{:keys [loc uri db]}]
+  (r.transform/expand-let loc uri db))
 
-(defmethod refactor :extract-function [{:keys [loc uri args] {:keys [db*]} :components}]
-  (apply r.transform/extract-function loc uri (concat args [@db*])))
+(defmethod refactor :extract-function [{:keys [loc uri args db]}]
+  (apply r.transform/extract-function loc uri (concat args [db])))
 
-(defmethod refactor :inline-symbol [{:keys [uri row col] {:keys [db*]} :components}]
-  (r.transform/inline-symbol uri row col @db*))
+(defmethod refactor :inline-symbol [{:keys [uri row col db]}]
+  (r.transform/inline-symbol uri row col db))
 
 (defmethod refactor :introduce-let [{:keys [loc args]}]
   (apply r.transform/introduce-let loc args))
 
-(defmethod refactor :move-to-let [{:keys [loc args uri] {:keys [db*]} :components}]
-  (apply r.transform/move-to-let loc uri @db* args))
+(defmethod refactor :move-to-let [{:keys [loc uri db args]}]
+  (apply r.transform/move-to-let loc uri db args))
 
-(defmethod refactor :thread-first [{:keys [loc] {:keys [db*]} :components}]
-  (r.transform/thread-first loc @db*))
+(defmethod refactor :thread-first [{:keys [loc db]}]
+  (r.transform/thread-first loc db))
 
-(defmethod refactor :thread-first-all [{:keys [loc] {:keys [db*]} :components}]
-  (r.transform/thread-first-all loc @db*))
+(defmethod refactor :thread-first-all [{:keys [loc db]}]
+  (r.transform/thread-first-all loc db))
 
-(defmethod refactor :thread-last [{:keys [loc] {:keys [db*]} :components}]
-  (r.transform/thread-last loc @db*))
+(defmethod refactor :thread-last [{:keys [loc db]}]
+  (r.transform/thread-last loc db))
 
-(defmethod refactor :thread-last-all [{:keys [loc] {:keys [db*]} :components}]
-  (r.transform/thread-last-all loc @db*))
+(defmethod refactor :thread-last-all [{:keys [loc db]}]
+  (r.transform/thread-last-all loc db))
 
 (defmethod refactor :unwind-all [{:keys [loc]}]
   (r.transform/unwind-all loc))
@@ -76,30 +76,29 @@
 (defmethod refactor :unwind-thread [{:keys [loc]}]
   (r.transform/unwind-thread loc))
 
-(defmethod refactor :resolve-macro-as [{:keys [loc uri components]}]
-  (let [{:keys [db* producer]} components]
-    (f.resolve-macro/resolve-macro-as! loc uri @db* producer components)))
+(defmethod refactor :resolve-macro-as [{:keys [loc uri db components]}]
+  (f.resolve-macro/resolve-macro-as! loc uri db components))
 
 (defmethod refactor :sort-map [{:keys [loc]}]
   (f.sort-map/sort-map loc))
 
-(defmethod refactor :move-coll-entry-up [{:keys [loc uri] {:keys [db*]} :components}]
-  (f.move-coll-entry/move-up loc uri @db*))
+(defmethod refactor :move-coll-entry-up [{:keys [loc uri db]}]
+  (f.move-coll-entry/move-up loc uri db))
 
-(defmethod refactor :move-coll-entry-down [{:keys [loc uri] {:keys [db*]} :components}]
-  (f.move-coll-entry/move-down loc uri @db*))
+(defmethod refactor :move-coll-entry-down [{:keys [loc uri db]}]
+  (f.move-coll-entry/move-down loc uri db))
 
 (defmethod refactor :suppress-diagnostic [{:keys [loc args]}]
   (apply r.transform/suppress-diagnostic loc args))
 
-(defmethod refactor :create-function [{:keys [loc uri] {:keys [db*]} :components}]
-  (r.transform/create-function loc uri @db*))
+(defmethod refactor :create-function [{:keys [loc uri db]}]
+  (r.transform/create-function loc uri db))
 
-(defmethod refactor :create-test [{:keys [loc uri] {:keys [db* producer]} :components}]
-  (r.transform/create-test loc uri @db* producer))
+(defmethod refactor :create-test [{:keys [loc uri db] {:keys [producer]} :components}]
+  (r.transform/create-test loc uri db producer))
 
-(defmethod refactor :move-form [{:keys [loc uri args] {:keys [db*]} :components}]
-  (apply f.move-form/move-form loc uri db* args))
+(defmethod refactor :move-form [{:keys [loc uri args db]}]
+  (apply f.move-form/move-form loc uri db args))
 
 (def available-refactors
   (->> refactor
@@ -115,7 +114,9 @@
 
 (defn call-refactor [{:keys [loc uri refactoring row col version] :as data} {:keys [db*] :as components}]
 
-  (let [result (refactor (assoc data :components components))]
+  (let [result (refactor (assoc data
+                                :components components
+                                :db @db*))]
     (cond
       (:no-op? result)
       nil
--- lib/src/clojure_lsp/feature/resolve_macro.clj
+++ lib/src/clojure_lsp/feature/resolve_macro.clj
@@ -67,7 +67,7 @@
    "clj-kondo.lint-as/def-catch-all"])
 
 (defn resolve-macro-as!
-  [zloc uri db producer components]
+  [zloc uri db {:keys [producer] :as components}]
   (let [project-root-uri (:project-root-uri db)
         resolved-full-symbol-str (producer/show-message-request producer "Select how LSP should resolve this macro:" :info (mapv #(hash-map :title %) known-full-symbol-resolve))
         kondo-config-paths-options [(lsp.kondo/project-config-path project-root-uri)

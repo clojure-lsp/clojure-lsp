09ccab51a1c8e3038dee45797d1d8b00521ca8d6
Jacob Maine <jacob.maine@gmail.com>
2022-04-13 15:29:14 -0400

    Optimize finding ns def by filename
    Reduces CPU usage of refresh-test-tree and probably code-lens by
    querying fewer elements. More noticeable on startup, where we refresh
    the test tree of each project file individually.
    
    refresh-test-tree can still take several seconds at startup, but now the
    majority of that time appears to be in `(.publishTestTree client tree)`,
    which is something to research later (it doesn't seem like it should be
    slow).
    
    In clj-kondo this patch cuts the initial refresh-test-tree time down
    from about 4-5 seconds to 2 or less.

--- lib/src/clojure_lsp/shared.clj
+++ lib/src/clojure_lsp/shared.clj
@@ -421,8 +421,9 @@
        result#)))
 
 (defmacro logging-task [task-id & body]
-  (let [msg (str task-id " %s")]
-    (with-meta `(logging-time ~msg ~@body) (meta &form))))
+  (with-meta `(let [msg# (str ~task-id " %s")]
+                (logging-time msg# ~@body))
+             (meta &form)))
 
 (defn ->range [{:keys [name-row name-end-row name-col name-end-col row end-row col end-col] :as element}]
   (when element

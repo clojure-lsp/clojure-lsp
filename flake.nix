cc442c96d3b2a5f4f49f0eca42fe9b0c9148a295
José Luis Lafuente <jl@lafuente.me>
2022-05-31 00:19:20 +0200

    Add flake with clj-nix

---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+  description = "Clojure LSP flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    clj-nix = {
+      url = "github:jlesquembre/clj-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils, clj-nix }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        cljpkgs = clj-nix.packages."${system}";
+      in
+      {
+        packages = {
+
+          cli-jdk = cljpkgs.mkCljBin {
+            projectSrc = ./.;
+            name = "com.github.clojure-lsp/clojure-lsp";
+            main-ns = "clojure-lsp.main";
+            jdkRunner = pkgs.jdk17_headless;
+            buildCommand =
+              ''
+                mkdir -p target
+                make cli-jar-for-native
+                cp clojure-lsp-standalone.jar target
+
+              '';
+            maven-extra = [{
+              content =
+                ''
+                  <?xml version="1.0" encoding="UTF-8"?>
+                  <metadata modelVersion="1.1.0">
+                    <groupId>com.google.code.gson</groupId>
+                    <artifactId>gson</artifactId>
+                    <versioning>
+                      <latest>2.9.0</latest>
+                      <release>2.9.0</release>
+                      <versions>
+                        <version>2.8.9</version>
+                        <version>2.9.0</version>
+                      </versions>
+                      <lastUpdated>19700101000000</lastUpdated>
+                    </versioning>
+                  </metadata>
+                '';
+              path = "com/google/code/gson/gson/maven-metadata-central.xml";
+            }];
+          };
+
+
+          cli = cljpkgs.mkGraalBin {
+            cljDrv = self.packages."${system}".cli-jdk;
+          };
+
+        };
+      });
+
+}

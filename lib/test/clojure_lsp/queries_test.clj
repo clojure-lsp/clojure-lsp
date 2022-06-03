clojure-lsp/clojure-lsp

commit d5e1ef39bc17cd8bb7f94bf6c9f611335dd6b2af

Fix find definition of macros required by :require-macros on cljs files.
Fixes #980

lib/src/clojure_lsp/queries.clj
@@ -140,7 +140,8 @@
           (= (:name %) (:name element))
           (not (= (:defined-by %) 'clojure.core/declare))
           (= (:ns %) (:to element))
-          (match-file-lang % element))
+          (or (contains? (elem-langs element) :cljs)
+              (match-file-lang % element)))
     analysis))
 
 (defmethod find-definition :local-usages

lib/test/clojure_lsp/queries_test.clj
@@ -455,7 +455,16 @@
         (q/find-definition-from-cursor ana (h/file-path "/b.cljc") bar-r-clj bar-c-clj))
       (h/assert-submap
         {:name 'bar :filename (h/file-path "/some.jar:other-jar.cljs")}
-        (q/find-definition-from-cursor ana (h/file-path "/b.cljc") bar-r-cljs bar-c-cljs)))))
+        (q/find-definition-from-cursor ana (h/file-path "/b.cljc") bar-r-cljs bar-c-cljs))))
+  (testing "when a macro is require-macros on cljs, being the var-definition on a clj/cljc file."
+    (h/clean-db!)
+    (h/load-code-and-locs (h/code "(ns some.foo) (defmacro bar [& body] @body)") (h/file-uri "file:///some/foo.clj"))
+    (h/load-code-and-locs (h/code "(ns some.foo (:require-macros [some.foo])) (def baz)") (h/file-uri "file:///some/foo.cljs"))
+    (let [[[bar-r bar-c]] (h/load-code-and-locs (h/code "(ns a (:require [some.foo :as f])) (f/|bar 1)") (h/file-uri "file:///a.cljs"))
+          ana (:analysis @db/db*)]
+      (h/assert-submap
+        {:name 'bar :filename (h/file-path "/some/foo.clj")}
+        (q/find-definition-from-cursor ana (h/file-path "/a.cljs") bar-r bar-c)))))
 
 (deftest find-definition-from-cursor-when-declared
   (let [[[bar-r bar-c]] (h/load-code-and-locs

commit c4b9f74f87c616a347aed852c21cb3eb1e05e2fb

Avoid going to clj def from cljs usage
```clojure
;; a.cljs
(ns a)
(def f)
;; a.clj
(ns a)
(def f)
;; b.cljs
(ns b
  (:require [a]))
|a/f
```

In the above code, find-definition should always go to `a.cljs`, but it
could go to `a.clj`. Which one it picked was arbitrary, because both
definitions match.

This bug was accidentally introduced in
d5e1ef39bc17cd8bb7f94bf6c9f611335dd6b2af, which was developed to fix
https://github.com/clojure-lsp/clojure-lsp/issues/980. It might have
been caught earlier because there are tests around this. Unfortunately,
the tests only fail if the cljs file is loaded before the clj file,
which isn't how they were written originally.

This patch also addresses 980, but avoids the incorrect navigation.

lib/src/clojure_lsp/queries.clj
@@ -135,14 +135,23 @@
 
 (defmethod find-definition :var-usages
   [analysis element]
-  (find-last-order-by-project-analysis
-    #(and (identical? :var-definitions (:bucket %))
-          (= (:name %) (:name element))
-          (not (= (:defined-by %) 'clojure.core/declare))
-          (= (:ns %) (:to element))
-          (or (contains? (elem-langs element) :cljs)
-              (match-file-lang % element)))
-    analysis))
+  (letfn [(defines-elem? [candidate]
+            (and (identical? :var-definitions (:bucket candidate))
+                 (= (:name element) (:name candidate))
+                 (= (:to element) (:ns candidate))
+                 (not= 'clojure.core/declare (:defined-by candidate))))]
+    (or
+      (find-last-order-by-project-analysis
+        #(and (defines-elem? %)
+              (match-file-lang % element))
+        analysis)
+      (when (contains? (elem-langs element) :cljs)
+        ;; maybe loaded by :require-macros, in which case, def will be in a clj file.
+        (find-last-order-by-project-analysis
+          #(and (defines-elem? %)
+                (:macro %)
+                (match-file-lang % (assoc element :lang :clj)))
+          analysis)))))
 
 (defmethod find-definition :local-usages
   [analysis {:keys [id filename] :as _element}]

lib/test/clojure_lsp/queries_test.clj
@@ -420,9 +420,7 @@
       {:name 'bar :filename (h/file-path "/a.clj")}
       (q/find-definition-from-cursor ana (h/file-path "/b.clj") bar-r bar-c))))
 
-(deftest find-definition-from-cursor-when-it-has-same-namespace-from-clj-and-cljs
-  (h/load-code-and-locs (h/code "(ns foo) (def bar)") (h/file-uri "jar:file:///some.jar!/some-jar.clj"))
-  (h/load-code-and-locs (h/code "(ns foo) (def bar)") (h/file-uri "jar:file:///some.jar!/other-jar.cljs"))
+(defn assert-find-definition-from-cursor-when-it-has-same-namespace-from-clj-and-cljs []
   (testing "when on a clj file"
     (let [[[bar-r bar-c]] (h/load-code-and-locs (h/code "(ns baz (:require [foo :as f]))"
                                                         "|f/bar") (h/file-uri "file:///b.clj"))
@@ -465,6 +463,16 @@
       (h/assert-submap
         {:name 'bar :filename (h/file-path "/some/foo.clj")}
         (q/find-definition-from-cursor ana (h/file-path "/a.cljs") bar-r bar-c)))))
+
+(deftest find-definition-from-cursor-when-it-has-same-namespace-from-clj-and-cljs
+  (h/load-code-and-locs (h/code "(ns foo) (def bar)") (h/file-uri "jar:file:///some.jar!/some-jar.clj"))
+  (h/load-code-and-locs (h/code "(ns foo) (def bar)") (h/file-uri "jar:file:///some.jar!/other-jar.cljs"))
+  (assert-find-definition-from-cursor-when-it-has-same-namespace-from-clj-and-cljs))
+
+(deftest find-definition-from-cursor-when-it-has-same-namespace-from-clj-and-cljs-in-other-load-order
+  (h/load-code-and-locs (h/code "(ns foo) (def bar)") (h/file-uri "jar:file:///some.jar!/other-jar.cljs"))
+  (h/load-code-and-locs (h/code "(ns foo) (def bar)") (h/file-uri "jar:file:///some.jar!/some-jar.clj"))
+  (assert-find-definition-from-cursor-when-it-has-same-namespace-from-clj-and-cljs))
 
 (deftest find-definition-from-cursor-when-declared
   (let [[[bar-r bar-c]] (h/load-code-and-locs
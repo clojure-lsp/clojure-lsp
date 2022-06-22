ab21529f0f5cc6e2b5543a1972e607b00d96bbcb
Michiel Borkent <michielborkent@gmail.com>
2022-06-22 21:19:19 +0200

    commit only on tag and brew

--- scripts/clojure_lsp/release_artifact.clj
+++ scripts/clojure_lsp/release_artifact.clj
@@ -11,22 +11,32 @@
           :out
           str/trim)))
 
+(defn tag []
+  (->> (sh "git tag --points-at HEAD")
+       :out
+       str/trim
+       not-empty))
+
 (defn release [& args]
-  (let [current-version (-> (slurp "lib/resources/CLOJURE_LSP_VERSION")
-                            str/trim)
-        ght (System/getenv "GITHUB_TOKEN")
-        file (first args)
-        branch (current-branch)
-        release-branch? (contains? #{"master" "main"} branch)]
-    (when-not ght
-      (println "Skipping: not GITHUB_TOKEN"))
-    (when-not release-branch?
-      (println "Skipping: not on release branch"))
-    (if (and ght release-branch?)
-      (do (assert file "File name must be provided")
-          (ghr/overwrite-asset {:org "clojure-lsp"
-                                :repo "clojure-lsp"
-                                :file file
-                                :tag (str "v" current-version)}))
-      (println "Skipping release artifact (no GITHUB_TOKEN or not on main branch)"))
-    nil))
+  (let [t (tag)]
+    (println "The tag: " t)
+    (if t
+      (let [current-version (-> (slurp "lib/resources/CLOJURE_LSP_VERSION")
+                                str/trim)
+            ght (System/getenv "GITHUB_TOKEN")
+            file (first args)
+            branch (current-branch)
+            release-branch? (contains? #{"master" "main"} branch)]
+        (when-not ght
+          (println "Skipping: not GITHUB_TOKEN"))
+        (when-not release-branch?
+          (println "Skipping: not on release branch"))
+        (if (and ght release-branch?)
+          (do (assert file "File name must be provided")
+              (ghr/overwrite-asset {:org "clojure-lsp"
+                                    :repo "clojure-lsp"
+                                    :file file
+                                    :tag (str "v" current-version)}))
+          (println "Skipping release artifact (no GITHUB_TOKEN or not on main branch)"))
+        nil)
+      (println "Skipping release artifact because no tag"))))

7879197270a2d55f9db514df25e9483133ea24b8
Jacob Maine <jacob.maine@gmail.com>
2022-06-30 14:21:04 -0700

    Extend tests of file-management (#1090)
    Adds tests for the async messages created by did-open, did-change and
    did-change-watched-files.
    
    Also adds integration test for did-change.

---
+++ cli/integration-test/integration/text_change_test.clj
@@ -0,0 +1,29 @@
+(ns integration.text-change-test
+  (:require
+   [clojure.test :refer [deftest testing is]]
+   [integration.fixture :as fixture]
+   [integration.lsp :as lsp]))
+
+(def sample-file-path "text_change/a.clj")
+
+(lsp/clean-after-test)
+
+(deftest apply-changes
+  (lsp/start-process!)
+  (lsp/request! (fixture/initialize-request))
+  (lsp/notify! (fixture/initialized-notification))
+  (lsp/notify! (fixture/did-open-notification sample-file-path))
+
+  (testing "Change is applied"
+    (is (= "original"
+           (-> (lsp/request! (fixture/hover-request sample-file-path 4 2))
+               :contents
+               (get 2))))
+
+    (lsp/notify! (fixture/did-change-notification sample-file-path 1 [["changed" 2 11 2 19]]))
+    (lsp/client-awaits-server-diagnostics sample-file-path)
+
+    (is (= "changed"
+           (-> (lsp/request! (fixture/hover-request sample-file-path 4 2))
+               :contents
+               (get 2))))))

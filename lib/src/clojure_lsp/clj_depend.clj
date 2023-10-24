d2ac43776013be2f93e357c7318d6d7c163042a4
Fabio Domingues <contato@fabiodomingues.com>
2023-10-24 09:41:25 -0300

    Supporting configuration to clj-depend via CLI

--- lib/src/clojure_lsp/clj_depend.clj
+++ lib/src/clojure_lsp/clj_depend.clj
@@ -4,23 +4,54 @@
    [clojure-lsp.logger :as logger]
    [clojure-lsp.settings :as settings]
    [clojure-lsp.shared :as shared]
-   [clojure.java.io :as io]
-   [medley.core :as medley]))
+   [clojure.java.io :as io]))
 
 (set! *warn-on-reflection* true)
 
-(defn relative-project-source-paths
+(defn ^:private violations-grouped-by-namespace
+  [{:keys [violations]}]
+  {:violations (group-by :namespace violations)})
+
+(defn ^:private analyze
+  [options]
+  (logger/info "Running clj-depend analysis with the following options:" options)
+  (try
+    (clj-depend/analyze options)
+    (catch Exception e
+      (logger/error "Error while running clj-depend analysis" e))))
+
+(defn ^:private with-files
+  [options paths]
+  (assoc options :files (set (map io/file paths))))
+
+(defn ^:private with-namespaces
+  [options namespace]
+  (assoc options :namespaces #{namespace}))
+
+(defn ^:private relative-project-source-paths
   [project-root db]
-  (map #(shared/relativize-filepath % project-root)
-       (settings/get db [:source-paths])))
+  (set (map #(shared/relativize-filepath % project-root)
+            (settings/get db [:source-paths]))))
 
-(defn config-with-source-paths
-  [config source-paths]
-  (if (not-empty (:source-paths config))
+(defn ^:private with-source-paths
+  [options project-root db]
+  (if (-> options :config :source-paths not-empty)
+    options
+    (assoc-in options [:config :source-paths] (relative-project-source-paths project-root db))))
+
+(defn ^:private with-project-root
+  [options project-root]
+  (assoc options :project-root (io/file project-root)))
+
+(defn ^:private new-options
+  "This function maintains backwards compatibility with the old and new configuration format"
+  [{:keys [snapshot?] :as config}]
+  (if (:config config)
     config
-    (assoc config :source-paths source-paths)))
+    (shared/assoc-some {:config (dissoc config :project-root :files :namespaces :snapshot?)}
+                       :snapshot? snapshot?)))
 
-(defn ^:private configured?*
+(defn ^:private configured?
   [project-root]
   (try
     (clj-depend/configured? (io/file project-root))
@@ -28,34 +59,35 @@
       (logger/error e "Error checking if clj-depend is configured on" project-root)
       false)))
 
-(defn ^:private configured?
+(defn ^:private should-run?
   [config project-root]
-  (or (seq config)
-      (configured?* project-root)))
+  (when project-root
+    (or config
+        (configured? project-root))))
 
 (defn analyze-uri! [uri db]
-  (when-let [project-root (some-> db :project-root-uri shared/uri->filename)]
-    (let [config (settings/get db [:clj-depend] {})]
-      (when (configured? config project-root)
-        ;; NOTE probably can't use dep-graph to find nses of uri, because
-        ;; dep-graph won't exist until after kondo analysis is done, which is
-        ;; run in parallel to clj-depend.
-        (when-let [namespace (some-> uri (shared/uri->namespace db) symbol)]
-          (-> {:violations {namespace []}}
-              (medley/deep-merge
-                (-> (clj-depend/analyze {:project-root (io/file project-root)
-                                         :config (config-with-source-paths config (relative-project-source-paths project-root db))
-                                         :namespaces #{namespace}})
-                    (update :violations #(group-by :namespace %))))))))))
+  (let [project-root (some-> db :project-root-uri shared/uri->filename)
+        config (not-empty (settings/get db [:clj-depend]))]
+    (when (should-run? config project-root)
+      ;; NOTE probably can't use dep-graph to find nses of uri, because
+      ;; dep-graph won't exist until after kondo analysis is done, which is
+      ;; run in parallel to clj-depend.
+      (when-let [namespace (some-> uri (shared/uri->namespace db) symbol)]
+        (-> (analyze (-> (new-options (or config {}))
+                         (with-project-root project-root)
+                         (with-source-paths project-root db)
+                         (with-namespaces namespace)))
+            (violations-grouped-by-namespace))))))
 
 (defn analyze-paths! [paths db]
-  (when-let [project-root (some-> db :project-root-uri shared/uri->filename)]
-    (let [config (settings/get db [:clj-depend] {})]
-      (when (configured? config project-root)
-        (-> (clj-depend/analyze {:project-root (io/file project-root)
-                                 :config (config-with-source-paths config (relative-project-source-paths project-root db))
-                                 :files (set (map io/file paths))})
-            (update :violations #(group-by :namespace %)))))))
+  (let [project-root (some-> db :project-root-uri shared/uri->filename)
+        config (not-empty (settings/get db [:clj-depend]))]
+    (when (should-run? config project-root)
+      (-> (analyze (-> (new-options (or config {}))
+                       (with-project-root project-root)
+                       (with-source-paths project-root db)
+                       (with-files paths)))
+          (violations-grouped-by-namespace)))))
 
 (defn db-with-results
   "Update `db` with clj-depend result."
--- lib/test/clojure_lsp/clj_depend_test.clj
+++ lib/test/clojure_lsp/clj_depend_test.clj
@@ -1,17 +1,179 @@
 (ns clojure-lsp.clj-depend-test
   (:require
+   [clj-depend.api :as clj-depend.api]
    [clojure-lsp.clj-depend :as clj-depend]
+   [clojure-lsp.shared :as shared]
+   [clojure-lsp.test-helper :as h]
+   [clojure.java.io :as io]
    [clojure.test :refer [deftest is testing]]))
 
-(deftest config-with-source-paths-filled-test
-  (testing "Should add source-paths to config when it is nil"
-    (is (= {:source-paths #{"src" "test"}}
-           (clj-depend/config-with-source-paths {} #{"src" "test"}))))
+(def ^:private project-root-uri (h/file-uri "file:///project"))
+(def ^:private project-root (io/file (h/file-path "/project")))
+(def ^:private source-paths [(h/file-path "/project/src")
+                             (h/file-path "/project/test")])
+(def ^:private foo-file-uri (h/file-uri "file:///project/src/foo.clj"))
+(def ^:private foo-file (io/file (h/file-path "/project/src/foo.clj")))
 
-  (testing "Should add source-paths to config when it is empty"
-    (is (= {:source-paths #{"src" "test"}}
-           (clj-depend/config-with-source-paths {:source-paths #{}} #{"src" "test"}))))
+(def ^:private clj-depend-legacy-config
+  {:layers {:foo {:namespaces      #{'foo}
+                  :accesses-layers #{}}
+            :bar {:namespaces      #{'bar}
+                  :accesses-layers #{:foo}}}})
 
-  (testing "Should not override source-paths when it is not nil or empty"
-    (is (= {:source-paths #{"src"}}
-           (clj-depend/config-with-source-paths {:source-paths #{"src"}} #{"src" "test"})))))
+(def ^:private clj-depend-config
+  {:config clj-depend-legacy-config})
+
+(defn ^:private clj-depend-settings
+  [& {:keys [namespaces files config snapshot?]}]
+  (shared/assoc-some {:project-root project-root
+                      :config       (-> (:config clj-depend-config)
+                                        (assoc :source-paths #{"src" "test"})
+                                        (merge config))}
+                     :namespaces namespaces
+                     :files files
+                     :snapshot? snapshot?))
+
+(defn ^:private clj-depend-settings-without-layer-config
+  [& args]
+  (-> (apply clj-depend-settings args)
+      (update :config dissoc :layers)))
+
+(defn ^:private clj-depend-analyze-response
+  [& violations]
+  {:result-code 1
+   :violations  violations
+   :message     "Any message"})
+
+(defn ^:private clj-depend-violations-grouped-by-namespace
+  [& violations]
+  {:violations (group-by :namespace violations)})
+
+(def ^:private foo-namespace-violation
+  {:namespace            'foo
+   :dependency-namespace 'bar
+   :layer                :c
+   :dependency-layer     :b
+   :message              "\"foo\" should not depend on \"bar\" (layer \":c\" on \":b\")"})
+
+(def ^:private bar-namespace-violation
+  {:namespace 'bar
+   :message   "Circular dependency between \"bar\" and \"bar\""})
+
+(defn ^:private verify-no-interactions
+  []
+  (fn [& _args]
+    (throw (Exception. "Mocked function called when it is expected to have no interactions"))))
+
+(defn ^:private mock-fn-when-called-with-params
+  [mocked-fn & expected-args]
+  (fn [& args]
+    (is (= args expected-args) "Mocked function should be called with the expected parameters")
+    (mocked-fn)))
+
+(deftest analyze-uri!-test
+  (testing "When clj-depend is configured in clojure-lsp"
+    (with-redefs [clj-depend.api/configured? (verify-no-interactions)
+                  clj-depend.api/analyze (mock-fn-when-called-with-params (constantly (clj-depend-analyze-response foo-namespace-violation))
+                                                                          (clj-depend-settings :namespaces #{'foo}))]
+      (is (= (clj-depend-violations-grouped-by-namespace foo-namespace-violation)
+             (clj-depend/analyze-uri! foo-file-uri {:project-root-uri project-root-uri
+                                                    :settings         {:source-paths source-paths
+                                                                       :clj-depend   clj-depend-config}})))))
+
+  (testing "When clj-depend is configured in clojure-lsp using legacy config"
+    (with-redefs [clj-depend.api/configured? (verify-no-interactions)
+                  clj-depend.api/analyze (mock-fn-when-called-with-params (constantly (clj-depend-analyze-response foo-namespace-violation))
+                                                                          (clj-depend-settings :namespaces #{'foo}))]
+      (is (= (clj-depend-violations-grouped-by-namespace foo-namespace-violation)
+             (clj-depend/analyze-uri! foo-file-uri {:project-root-uri project-root-uri
+                                                    :settings         {:source-paths source-paths
+                                                                       :clj-depend   clj-depend-legacy-config}})))))
+
+  (testing "When clj-depend is configured in clojure-lsp with custom source-paths"
+    (with-redefs [clj-depend.api/configured? (verify-no-interactions)
+                  clj-depend.api/analyze (mock-fn-when-called-with-params (constantly (clj-depend-analyze-response foo-namespace-violation))
+                                                                          (clj-depend-settings :namespaces #{'foo}
+                                                                                               :config {:source-paths #{"custom-src"}}))]
+      (is (= {:violations {'foo [foo-namespace-violation]}}
+             (clj-depend/analyze-uri! foo-file-uri {:project-root-uri project-root-uri
+                                                    :settings         {:source-paths source-paths
+                                                                       :clj-depend   (assoc-in clj-depend-config [:config :source-paths] #{"custom-src"})}})))))
+
+  (testing "When clj-depend is configured in clojure-lsp and receives snapshot request"
+    (with-redefs [clj-depend.api/configured? (verify-no-interactions)
+                  clj-depend.api/analyze (mock-fn-when-called-with-params (constantly (clj-depend-analyze-response foo-namespace-violation))
+                                                                          (clj-depend-settings :namespaces #{'foo}
+                                                                                               :snapshot? true))]
+
+      (is (= {:violations {'foo [foo-namespace-violation]}}
+             (clj-depend/analyze-uri! foo-file-uri {:project-root-uri project-root-uri
+                                                    :settings         {:source-paths source-paths
+                                                                       :clj-depend   (assoc-in clj-depend-config [:snapshot?] true)}})))))
+
+  (testing "When clj-depend is configured but not through clojure-lsp"
+    (with-redefs [clj-depend.api/configured? (mock-fn-when-called-with-params (constantly true)
+                                                                              project-root)
+                  clj-depend.api/analyze (mock-fn-when-called-with-params (constantly (clj-depend-analyze-response foo-namespace-violation))
+                                                                          (clj-depend-settings-without-layer-config :namespaces #{'foo}))]
+      (is (= {:violations {'foo [foo-namespace-violation]}}
+             (clj-depend/analyze-uri! foo-file-uri {:project-root-uri project-root-uri
+                                                    :settings         {:source-paths source-paths}}))))))
+
+(deftest analyze-paths!-test
+  (testing "When clj-depend is configured in clojure-lsp"
+    (with-redefs [clj-depend.api/configured? (verify-no-interactions)
+                  clj-depend.api/analyze (mock-fn-when-called-with-params (constantly (clj-depend-analyze-response foo-namespace-violation))
+                                                                          (clj-depend-settings :files #{foo-file}))]
+      (is (= (clj-depend-violations-grouped-by-namespace foo-namespace-violation)
+             (clj-depend/analyze-paths! [foo-file] {:project-root-uri project-root-uri
+                                                    :settings         {:source-paths source-paths
+                                                                       :clj-depend   clj-depend-config}})))))
+
+  (testing "When clj-depend is configured in clojure-lsp using legacy config"
+    (with-redefs [clj-depend.api/configured? (verify-no-interactions)
+                  clj-depend.api/analyze (mock-fn-when-called-with-params (constantly (clj-depend-analyze-response foo-namespace-violation))
+                                                                          (clj-depend-settings :files #{foo-file}))]
+      (is (= (clj-depend-violations-grouped-by-namespace foo-namespace-violation)
+             (clj-depend/analyze-paths! [foo-file] {:project-root-uri project-root-uri
+                                                    :settings         {:source-paths source-paths
+                                                                       :clj-depend   clj-depend-legacy-config}})))))
+
+  (testing "When clj-depend is configured in clojure-lsp with custom source-paths"
+    (with-redefs [clj-depend.api/configured? (verify-no-interactions)
+                  clj-depend.api/analyze (mock-fn-when-called-with-params (constantly (clj-depend-analyze-response foo-namespace-violation))
+                                                                          (clj-depend-settings :files #{foo-file}
+                                                                                               :config {:source-paths #{"custom-src"}}))]
+      (is (= {:violations {'foo [foo-namespace-violation]}}
+             (clj-depend/analyze-paths! [foo-file] {:project-root-uri project-root-uri
+                                                    :settings         {:source-paths source-paths
+                                                                       :clj-depend   (assoc-in clj-depend-config [:config :source-paths] #{"custom-src"})}})))))
+
+  (testing "When clj-depend is configured in clojure-lsp and receives snapshot request"
+    (with-redefs [clj-depend.api/configured? (verify-no-interactions)
+                  clj-depend.api/analyze (mock-fn-when-called-with-params (constantly (clj-depend-analyze-response foo-namespace-violation))
+                                                                          (clj-depend-settings :files #{foo-file}
+                                                                                               :snapshot? true))]
+
+      (is (= {:violations {'foo [foo-namespace-violation]}}
+             (clj-depend/analyze-paths! [foo-file] {:project-root-uri project-root-uri
+                                                    :settings         {:source-paths source-paths
+                                                                       :clj-depend   (assoc-in clj-depend-config [:snapshot?] true)}})))))
+
+  (testing "When clj-depend is configured but not through clojure-lsp"
+    (with-redefs [clj-depend.api/configured? (mock-fn-when-called-with-params (constantly true)
+                                                                              project-root)
+                  clj-depend.api/analyze (mock-fn-when-called-with-params (constantly (clj-depend-analyze-response foo-namespace-violation))
+                                                                          (clj-depend-settings-without-layer-config :files #{foo-file}))]
+      (is (= {:violations {'foo [foo-namespace-violation]}}
+             (clj-depend/analyze-paths! [foo-file] {:project-root-uri project-root-uri
+                                                    :settings         {:source-paths source-paths}}))))))
+
+(deftest db-with-results-test
+  (testing "Should return db with :clj-depend-violations"
+    (is (= {:clj-depend-violations {'foo foo-namespace-violation}}
+           (clj-depend/db-with-results {} {:violations {'foo foo-namespace-violation}}))))
+
+  (testing "Should return db with :clj-depend-violations when there are already violations"
+    (is (= {:clj-depend-violations {'foo foo-namespace-violation
+                                    'bar bar-namespace-violation}}
+           (clj-depend/db-with-results {:clj-depend-violations {'foo foo-namespace-violation}} {:violations {'bar bar-namespace-violation}})))))

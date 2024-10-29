767e1198203d57cde31dd9076fbfb6733e64af8f
André Camargo <andre.camargo@nubank.com.br>
2024-10-29 07:17:12 -0300

    :nail_care:

--- lib/src/clojure_lsp/snapshot.clj
+++ lib/src/clojure_lsp/snapshot.clj
@@ -2,27 +2,7 @@
   (:require
    [clojure-lsp.shared :as shared]
    [clojure.java.io :as io]
-   [clojure.string :as str])
-  (:import
-   (java.security MessageDigest)
-   [java.util UUID]))
-
-(defn string-to-uuid [s]
-  (let [md5 (MessageDigest/getInstance "MD5")
-        hash (.digest md5 (.getBytes s "UTF-8"))
-        msb (->> (take 8 hash)
-                 (map #(bit-and % 0xFF))
-                 (reduce (fn [acc b] (bit-or (bit-shift-left acc 8) b)) 0))
-        lsb (->> (drop 8 hash)
-                 (map #(bit-and % 0xFF))
-                 (reduce (fn [acc b] (bit-or (bit-shift-left acc 8) b)) 0))]
-    (UUID. msb lsb)))
-
-(defn ^:private string-to-md5
-  [input-str]
-  (let [algorithm (MessageDigest/getInstance "MD5")
-        raw (.digest algorithm (.getBytes input-str "UTF-8"))]
-    (format "%032x" (BigInteger. 1 raw))))
+   [clojure.string :as str]))
 
 (defn ^:private read-file
   ([] (read-file (io/file ".lsp" "snapshot.txt")))

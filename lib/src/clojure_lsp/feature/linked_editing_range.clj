62b154fcc807bcd49afbe86a8adfc82a8306bef8
Paul Butcher <paul@paulbutcher.com>
2025-01-14 13:32:21 +0000

    Restrict linked edits to namespace aliases only

--- lib/src/clojure_lsp/feature/linked_editing_range.clj
+++ lib/src/clojure_lsp/feature/linked_editing_range.clj
@@ -6,13 +6,8 @@
 (set! *warn-on-reflection* true)
 
 (defn ranges [uri row col db]
-  (let [elements (q/find-references-from-cursor db uri row col true)
-        same-file-references-only? (= 1 (count (keys (group-by :uri elements))))]
-    (if same-file-references-only?
-      {:ranges (->> elements
-                    (map shared/->range))}
-      (if (seq elements)
-        {:error {:code :invalid-params
-                 :message "There are references on other files for this symbol"}}
-        {:error {:code :invalid-params
-                 :message "No symbol found"}}))))
+  (when-let [element (q/find-element-under-cursor db uri row col)]
+    (when (= :namespace-alias (:bucket element))
+      (when-let [elements (q/find-references db element true)]
+        {:ranges (->> elements
+                      (map shared/->range))}))))
--- lib/test/clojure_lsp/feature/linked_editing_range_test.clj
+++ lib/test/clojure_lsp/feature/linked_editing_range_test.clj
@@ -2,32 +2,27 @@
   (:require
    [clojure-lsp.feature.linked-editing-range :as f.linked-editing-range]
    [clojure-lsp.test-helper :as h]
-   [clojure.test :refer [deftest testing]]))
+   [clojure.test :refer [deftest testing is]]))
 
 (h/reset-components-before-test)
 
 (deftest ranges
-  (h/load-code-and-locs
-    (h/code "(ns foo)"
-            ":foo"
-            "(defn foo [a] a)"
-            "(defn some-fn [a] a)"
-            "(foo 1)"
-            "some-fn"
-            "foo"))
-  (h/load-code-and-locs
-    (h/code "(ns bar"
-            " (:require [foo :as f]))"
-            "(f/some-fn 3)"
-            "") "file:///b.clj")
-  (testing "when all references are on the same file"
-    (h/assert-submap
-      {:ranges [{:start {:line 2, :character 6}, :end {:line 2, :character 9}}
-                {:start {:line 4, :character 1}, :end {:line 4, :character 4}}
-                {:start {:line 6, :character 0}, :end {:line 6, :character 3}}]}
-      (f.linked-editing-range/ranges h/default-uri 5 2 (h/db))))
-  (testing "when some reference is on another file"
-    (h/assert-submap
-      {:error {:code :invalid-params
-               :message "There are references on other files for this symbol"}}
-      (f.linked-editing-range/ranges h/default-uri 6 2 (h/db)))))
+  (let [[[alias-r alias-c]
+         [ref-r ref-c]
+         [other-r other-c]]
+        (h/load-code-and-locs
+          (h/code "(ns foo"
+                  "  (:require [bar :as b|az]))"
+                  "(def xyz 2)"
+                  "(ba|z/some-fn xy|z)"
+                  "(baz/some-other-fn 4)"))]
+    (testing "aliases are linked"
+      (h/assert-submap
+        {:ranges [{:start {:line 1, :character 21}, :end {:line 1, :character 24}}
+                  {:start {:line 3, :character 1}, :end {:line 3, :character 4}}
+                  {:start {:line 4, :character 1}, :end {:line 4, :character 4}}]}
+        (f.linked-editing-range/ranges h/default-uri alias-r alias-c (h/db))))
+    (testing "but not in the other direction"
+      (is (nil? (f.linked-editing-range/ranges h/default-uri ref-r ref-c (h/db)))))
+    (testing "and not for other symbols"
+      (is (nil? (f.linked-editing-range/ranges h/default-uri other-r other-c (h/db)))))))

23e51d9ebf907b6cf5357c2e0a62e9db1adcc5f8
Paul Butcher <paul@paulbutcher.com>
2025-01-15 12:36:59 +0000

    Linked edit fixes (#1947)
    * Restrict linked edits to namespace aliases only
    
    * Use alias-range instead of ->range
    
    * Remove debugging erroneously left in
    
    * Fix integration tests
    
    * Fix lint
    
    * Update changelog

--- lib/src/clojure_lsp/feature/linked_editing_range.clj
+++ lib/src/clojure_lsp/feature/linked_editing_range.clj
@@ -5,14 +5,14 @@
 
 (set! *warn-on-reflection* true)
 
+(defn ^:private alias-range [alias-length {:keys [name-row name-col]}]
+  {:start (shared/row-col->position name-row name-col)
+   :end (shared/row-col->position name-row (+ name-col alias-length))})
+
 (defn ranges [uri row col db]
-  (let [elements (q/find-references-from-cursor db uri row col true)
-        same-file-references-only? (= 1 (count (keys (group-by :uri elements))))]
-    (if same-file-references-only?
-      {:ranges (->> elements
-                    (map shared/->range))}
-      (if (seq elements)
-        {:error {:code :invalid-params
-                 :message "There are references on other files for this symbol"}}
-        {:error {:code :invalid-params
-                 :message "No symbol found"}}))))
+  (when-let [element (q/find-element-under-cursor db uri row col)]
+    (when (= :namespace-alias (:bucket element))
+      (when-let [elements (q/find-references db element true)]
+        (let [alias-length (count (str (:alias element)))]
+          {:ranges (->> elements
+                        (map (partial alias-range alias-length)))})))))

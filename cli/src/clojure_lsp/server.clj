4b0cade33f47be36f446b6484d1c14ff4dce38e9
Eric Dallo <ericdallo06@hotmail.com>
2025-02-26 09:56:18 -0300

    Add client-info to db on initialize

--- cli/src/clojure_lsp/server.clj
+++ cli/src/clojure_lsp/server.clj
@@ -520,6 +520,7 @@
                       (some-> (:root-uri params) normalize-uri)
                       (:capabilities params)
                       (client-settings params)
+                      (:client-info params)
                       (some-> params :work-done-token str))
   (when-let [trace-level (:trace params)]
     (lsp.server/set-trace-level server trace-level))

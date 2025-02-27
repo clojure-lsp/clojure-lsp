72fa56fe477db7d44b29b803164caca32ea8e415
André Camargo <andre.camargo@nubank.com.br>
2025-02-27 07:51:59 -0300

    Make code easier to read

--- lib/test/clojure_lsp/feature/paredit_test.clj
+++ lib/test/clojure_lsp/feature/paredit_test.clj
@@ -2,8 +2,7 @@
   (:require
    [clojure-lsp.feature.paredit :as f.paredit]
    [clojure-lsp.test-helper :as h]
-   [clojure.string :as string]
-   [clojure.test :refer [deftest is]]
+   [clojure.test :refer [are deftest is]]
    [rewrite-clj.zip :as z]))
 
 (defmacro ^:private assert-op [op expected code]
@@ -12,36 +11,15 @@
      (is (= text#
             (h/changes->code (-> applied# :changes-by-uri first second) (h/db))))))
 
-(defn ^:private position-cursor
-  [s row col]
-  (let [lines (string/split s #"\n" -1)
-        line (nth lines (dec row))]
-    (string/join "\n"
-                 (concat
-                   (subvec lines 0 (dec row))
-                   [(str (subs line 0 (dec col))
-                         "|"
-                         (subs line (dec col)))]
-                   (subvec lines row)))))
-
-(defmacro ^:private assert-op-pos
-  [op expected code]
-  `(let [{{row# :row col# :col} :position zloc# :zloc} (h/load-code-into-zloc-and-position ~code)
-         applied# (~op h/default-uri
-                       zloc#
-                       row#
-                       col#)
-         {{actual-row# :row actual-col# :col} :range} (:show-document-after-edit applied#)
-         actual# (h/changes->code (-> applied#
-                                      :changes-by-uri
-                                      first
-                                      second)
-                                  (h/db))
-         actual-string# (if applied#
-                          (position-cursor actual# actual-row# actual-col#)
-                          "|")]
-     (is (= ~expected
-            actual-string#))))
+(defn ^:private edit
+  [paredit-fn code]
+  (let [{{row :row col :col} :position zloc :zloc} (h/load-code-into-zloc-and-position code)
+        transformations (paredit-fn h/default-uri zloc row col)
+        {{actual-row :row actual-col :col} :range} (:show-document-after-edit transformations)
+        result (h/changes->code (-> transformations :changes-by-uri first second) (h/db))]
+    (if transformations
+      (h/cursor-at result actual-row actual-col)
+      "|")))
 
 (comment
   (require '[rewrite-clj.paredit :as paredit]
@@ -133,62 +111,20 @@
                               (h/code "(get {}|) :a")))
   #_())
 
-(deftest slurp-forward-test
-  (assert-op-pos f.paredit/forward-slurp
-                 (h/code "|")
-                 (h/code "|"))
-  #_(let [code (h/code "(get []|) 0")
-          {{:keys [row col]} :position zloc :zloc} (h/load-code-into-zloc-and-position code)
-          applied (f.paredit/forward-slurp h/default-uri
-                                           zloc
-                                           row
-                                           col)
-          _ (println 'applied= applied)
-          {{actual-row :row actual-col :col} :range} (:show-document-after-edit applied)
-          actual (h/changes->code (-> applied
-                                      :changes-by-uri
-                                      first
-                                      second)
-                                  (h/db))
-          expected "(get []| 0)"]
-      (is (= (h/positions-from-text expected)
-             [actual [[actual-row actual-col]]]))))
-
 (deftest forward-slurp-test
-  (assert-op-pos f.paredit/forward-slurp
-                 (h/code "[1 [2 3| 4]]")
-                 (h/code "[1 [2 3|] 4]"))
-  (assert-op-pos f.paredit/forward-slurp
-                 (h/code "[1 [2 [|3 4 5]] 6]")
-                 (h/code "[1 [2 [|3 4] 5] 6]"))
-  (assert-op-pos f.paredit/forward-slurp
-                 (h/code "[|1 2]")
-                 (h/code "[|1] 2"))
-  (assert-op-pos f.paredit/forward-slurp
-                 (h/code "|1")
-                 (h/code "|1"))
-  (assert-op-pos f.paredit/forward-slurp
-                 (h/code "|")
-                 (h/code "|"))
-  (assert-op-pos f.paredit/forward-slurp
-                 (h/code "{:a {|:b 1} 2}")
-                 (h/code "{:a {|:b 1}} 2"))
-  (assert-op-pos f.paredit/forward-slurp
-                 (h/code "{:a {:b {|:c :d}} 2}")
-                 (h/code "{:a {:b {|:c :d}}} 2"))
-  ;; These two below are going to fail because of a bug in rewrite-clj
-  ;; (assert-op2 f.paredit/forward-slurp
-  ;;             (h/code "(get {}| :a)")
-  ;;             (h/code "(get {}|) :a"))
-  ;; (assert-op2 f.paredit/forward-slurp
-  ;;               (h/code "(get {} |:a)")
-  ;;               (h/code "(get {} |) :a"))
-  (assert-op-pos f.paredit/forward-slurp
-                 (h/code "(get []| 0)")
-                 (h/code "(get []|) 0"))
-  (assert-op-pos f.paredit/forward-slurp
-                 (h/code "(get [] |0)")
-                 (h/code "(get [] |) 0")))
+  (are [expected code] (= expected (edit f.paredit/forward-slurp code))
+    "[1 [2 3| 4]]" "[1 [2 3|] 4]"
+    "[1 [2 [|3 4 5]] 6]" "[1 [2 [|3 4] 5] 6]"
+    "[|1 2]" "[|1] 2"
+    "|1" "|1"
+    "|" "|"
+    "{:a {|:b 1} 2}" "{:a {|:b 1}} 2"
+    "{:a {:b {|:c :d}} 2}" "{:a {:b {|:c :d}}} 2"
+    ;; These two below are going to fail because of a bug in rewrite-clj
+    ;; "(get {}| :a)" "(get {}|) :a"
+    ;; "(get {} |:a)" "(get {} |) :a"
+    "(get []| 0)" "(get []|) 0"
+    "(get [] |0)" "(get [] |) 0"))
 
 (deftest forward-barf-test
   (assert-op f.paredit/forward-barf

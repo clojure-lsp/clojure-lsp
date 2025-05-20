d10bef35c1d5ff643c34cfb7329987039d07c035
Eric Dallo <ericdallo06@hotmail.com>
2025-05-20 11:22:55 -0300

    Finish API

--- test-helper/src/clojure_lsp/test_helper.clj
+++ test-helper/src/clojure_lsp/test_helper.clj
@@ -5,9 +5,34 @@
 
 (set! *warn-on-reflection* true)
 
-(defn load-code
+(def default-uri h.internal/default-uri)
+
+(def components* h.internal/components*)
+
+(defn db*
+  "The in-memory clojure-lsp db."
+  []
+  (h.internal/db*))
+
+(defn reset-components!
+  "Resets in-memory clojure-lsp components to a empty state,
+   removing any previously loaded code."
+  []
+  (h.internal/reset-components!))
+
+(defn code
+  "Return a string of concatenated strings separated by \n."
+  [& strings]
+  (apply h.internal/code strings))
+
+(defn load-code!
   "Simulates code load by clojure-lsp, analyzing it."
   [{:keys [code uri components]
     :or {uri h.internal/default-uri
          components h.internal/components}}]
   (h.internal/load-code code (h.internal/file-uri uri) components))
+
+(defn set-settings!
+  "Changes db adding specified settings."
+  [settings]
+  (swap! (h.internal/db*) assoc :settings settings))
